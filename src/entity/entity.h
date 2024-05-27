0c231ee5b594ff277ee8f411da1b3fb1e7301764
Josso Celia <celia.josso@etudiant.univ-rennes1.fr>
2024-05-27 12:19:31 +0000

    3-un-joueur

---
+++ src/entity/entity.h
@@ -0,0 +1,27 @@
+//
+// Created by cejosso on 27/05/2024.
+//
+# pragma once
+
+class entity {
+public :
+    entity(float x, float y, float width, float height);
+
+    virtual ~entity();
+
+    virtual void draw() = 0;
+    virtual void update() = 0;
+
+    void move(float dx, float dy);
+    void move_to(float x, float y);
+
+protected :
+    struct {
+        float x;
+        float y;
+    } position;
+    struct {
+        float width;
+        float height;
+    } size;
+};
