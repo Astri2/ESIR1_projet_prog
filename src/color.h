d997006c38b6f1bf50e0e88d984398063726306b
Vianney HERVÉ <debota@winota>
2024-05-27 15:48:27 +0200

    moved renderer to static interface

---
+++ src/color.h
@@ -0,0 +1,25 @@
+//
+// Created by viann on 27/05/2024.
+//
+#pragma once
+
+#include <cstdint>
+
+namespace renderer {
+
+    struct color {
+        uint8_t r, g, b, a;
+    };
+
+    namespace colors {
+
+        constexpr color white = { 255, 255, 255, 255 };
+        constexpr color black = { 0, 0, 0, 255 };
+
+        constexpr color red = { 255, 0, 0, 255 };
+        constexpr color green = { 0, 255, 0, 255 };
+        constexpr color blue = { 0, 0, 255, 255 };
+
+    } // namespace colors
+
+} // namespace renderer
