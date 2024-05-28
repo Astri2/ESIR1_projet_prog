3b846dc11155ce4602d5fd0fa892c9a3bb97014f
Chauvel Tom <tom.chauvel@etudiant.univ-rennes1.fr>
2024-05-28 22:57:55 +0000

    Vianney/draw map

--- src/map/map.h
+++ src/map/map.h
@@ -1,31 +1,29 @@
 //
 // Created by tomch on 28/05/2024.
 //
-
 #pragma once
 
-#include "config.h"
-#include "utils/vec2.h"
-#include "cluster.h"
+
 #include "camera.h"
-#include <vector>
+#include "cluster.h"
+#include "entity/player.h"
+#include "physics/physics.h"
 
-class map {
+namespace map
+{
+    extern player* p;
+    extern camera cam;
 
-public:
-    map(unsigned int _width, unsigned int _height);
-
-    void load_wsv(vec2<float> size, const char * file);
+    void load_wsv(const char* file);
 
     void draw();
-    void update();
+    void update(float dt);
+    uint32_t find_cluster_idx(const vec2<float>& position);
+    std::vector<cluster*> get_surrounding_clusters(uint32_t cluster_idx);
 
-protected:
-    unsigned int find_cluster_idx(const vec2<float>& position) const;
-    std::vector<cluster*> get_surrounding_clusters(unsigned int cluster_idx);
-    std::vector<cluster const*> get_cluster_to_blit(const camera & camera) const;
 
-protected:
-    std::vector<cluster> clusters;
-    unsigned int nb_clusters_x, nb_clusters_y;
-};
+    extern std::vector<cluster> clusters;
+    uint32_t find_cluster_idx(const vec2<float>& position);
+
+    interactible* perceive(player* user, std::vector<cluster*> clusters);
+} // namespace map
