38147f33b1bc3b17880459c5785ec4889c86ac9d
CelJos <celia.josso@orange.fr>
2024-05-28 13:30:34 +0200

    Mise en place de la map

---
+++ resources/png_to_csv.py
@@ -0,0 +1,25 @@
+from PIL import Image
+
+im = Image.open('map.png') # Can be many different formats.
+pix = im.load()
+
+out = ""
+col_to_texture = {
+    (112, 72, 24, 255): "dirt",
+    (255, 229, 0, 255): "sand",
+    (50, 80, 26, 255): "forest",
+    (105, 165, 57, 255): "grass",
+    (0, 253, 255, 255): "water",
+    (52, 104, 105, 255): "deep_water",
+}
+
+
+TILE_WIDTH, TILE_HEIGHT = 16, 16
+width, height = im.size
+
+for y in range(height):
+    for x in range(width):
+        out += f"tile {x} {y} {col_to_texture[pix[x, y]]}\n"
+
+with open("out.txt", 'w') as f:
+    f.write(out)
