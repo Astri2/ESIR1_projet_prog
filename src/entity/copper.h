44b777c93aff7d075114a0a5d87a7201c21b3d8f
Tom Chauvel <tom.chauvel@proton.me>
2024-05-29 13:33:07 +0200

    fix

--- src/entity/copper.h
+++ src/entity/copper.h
@@ -13,7 +13,7 @@ class copper : public crop, public sprite{
     public:
         copper(vec2<float> pos, vec2<float> size) :
                 entity(pos), crop(pos, size, 2),
-                sprite(pos, {{17,18}}, {{0, 0}}, {{17, 18}}, {{size.x / 2, size.y / 2}},
+                sprite(pos, {{18,17}}, {{0, 0}}, {{18, 17}}, {{size.x / 2, size.y / 2}},
                 "../resources/copper.png"){}
 
                 void interact(player *user) override;
