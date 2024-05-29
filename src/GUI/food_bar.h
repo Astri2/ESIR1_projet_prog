600c3d3f8f8ad97faa9060881315234ee445f457
CelJos <celia.josso@orange.fr>
2024-05-29 02:06:51 +0200

    created food_bar

---
+++ src/GUI/food_bar.h
@@ -0,0 +1,22 @@
+//
+// Created by celia on 29/05/2024.
+//
+
+#pragma once
+
+#include "camera.h"
+#include "gui_component.h"
+#include "entity/player.h"
+
+class food_bar : public gui_component {
+public :
+    food_bar(vec2<float> _position, vec2<float> _size, vec2<int> _sprite_offset , vec2<int> _sprite_resolution, const char* image_src, player * p);
+
+    void draw(const camera& cam) const override;
+    void update(float dt) override;
+
+private:
+    const player * p;
+    float current_food;
+    float max_food;
+};
